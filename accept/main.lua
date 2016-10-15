93809147172802d7408dfb20965f2e3a282e2ea1
koubmi <bouk555@gmail.com>
2016-10-15 01:39:02 -0700

    Camera scrolling and temp background images

--- accept/main.lua
+++ accept/main.lua
@@ -1,62 +1,6 @@
-
---Player--
-player = {}
-player.x = 50 --Spawn X-cord
-player.y = 490 --Spawn Y-cord
-player.speed = 5 --Speed per tick (x-cord)
+require("player")
 
 
---Jump logic
-player.canJump = true -- Can the player jump?
-player.jumpTicks = 30 --Ticks that each jump lasts
-player.jumpTicksLeft = 0 --Ticks left until stop jump
-player.jumpPerTick = 4 --Increase in Y per tick while jumping
-
-player.gravityPerTick = -3 -- Decrease in Y when not colliding or jumping
-
-player.update = function()
-
-	--Jump Logic
-	if player.jumpTicksLeft > 0 then
-		player.y = player.y - player.jumpPerTick
-		player.jumpTicksLeft = player.jumpTicksLeft - 1
-	elseif player.canJump and love.keyboard.isDown("up") then
-		player.canJump = false
-		player.jumpTicksLeft = player.jumpTicks
-	elseif player.y + 50 < 550 then
-		player.y = player.y - player.gravityPerTick
-	else
-		player.canJump = true
-	end
-	player.move()
-end
-
-player.move = function()
-
-	if love.keyboard.isDown("left") then
-		player.x = player.x - player.speed
-	end
-
-	if love.keyboard.isDown("right") then
-		player.x = player.x + player.speed
-	end
-
-	if love.keyboard.isDown("up") then
-		player.jump()
-	end
-
-end
-
-player.jump = function()
-
-	if player.currentJumpCD == 0 then
-		player.y = player.y - player.jumpHeight
-		player.currentJumpCD = player.jumpCD
-	end	
-
-end
-
---Environment Stuff
 enviro = {}
 
 cloud = {}
@@ -91,6 +35,7 @@ function love.load()
 --player.animation = love.graphics.newImage("test.png")
 
 --Clouds, etc.
+backgroundImage = love.graphics.newImage("back2.jpg")
 cloud.image = love.graphics.newImage("cloud.png")
 table.insert(enviro,cloud)
 
@@ -99,11 +44,10 @@ end
 function love.draw()
 
 	--Background
-	love.graphics.setColor(200,10,10)
-	love.graphics.rectangle("fill",0,0,800,600)
+	love.graphics.draw(backgroundImage, 0, 0, 0, 1, 1, backgroundX, backgroundY, 0, 0)
 
 	--Player
-	love.graphics.setColor(125,0,60)
+	--love.graphics.setColor(125,0,60)
 	--love.graphics.draw(player.animation, player.x, player.y, 0, 1, 1, 0, 0, 0, 0) 
 	love.graphics.rectangle("fill", player.x, player.y, 50, 100)
 
