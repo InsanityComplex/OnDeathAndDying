2d77385f340f61e05d410d995c2bcb301af007dc
crynth <jsm3@email.arizona.edu>
2016-10-15 12:44:40 -0700

    trying to fix untracked files

---
+++ anger/player.lua
@@ -0,0 +1,16 @@
+love.load()
+player = {}
+player.width = 15
+player.height = 30
+player.x = WINDOW_WIDTH / 2 - 15
+player.y = WINDOW_HEIGHT / 2 - 15
+player.speed = 10
+player.img = nil
+
+function player.update()
+  
+end
+
+function player.draw()
+  
+end
