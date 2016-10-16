d6b33c151c1669a04503d11ed34b8910f4e2c362
Daniel Tweedy <melbrookska@user.noreply.github.com>
2016-10-16 07:30:26 -0700

    Moved Player Animations to File
    Moved player animation tables to "animation.lua" and added them to a
    load function.

--- player.lua
+++ player.lua
@@ -1,3 +1,4 @@
+require "animation"
 --Player--
 player = {}
 
@@ -14,6 +15,9 @@ player.load = function()
 	player.bottomCollision = false
 	player.leftCollision = false
 	player.rightCollision = false
+
+  -- Animations
+  animation.player.load();
 
 	--Jump logic
   player.amountjumps = 2
@@ -40,32 +44,6 @@ player.load = function()
 	backgroundY = 0
 
 end
-
---Load images, move to load function soon
-player.falling = {}
-for i=1,40 do
-	player.falling[i-1] = love.graphics.newImage("Sprites/Player/Falling/" .. string.format("%04d",i) .. ".png")
-end
-
-
-player.running = {}
-for i=1,28 do
-	player.running[i-1] = love.graphics.newImage("Sprites/Player/running/" .. string.format("%04d",i) .. ".png")
-end
-
-player.idle = {}
-for i=1,60 do
-	player.idle[i-1] = love.graphics.newImage("Sprites/Player/idle/" .. string.format("%04d",i) .. ".png")
-end
-
-player.crouch = {}
-for i=1,10 do
-	player.crouch[i-1] = love.graphics.newImage("Sprites/Player/Crouch/" .. string.format("%04d",i) .. ".png")
-end
-
-player.jumpA = {}
-player.jumpA[0] = love.graphics.newImage("Sprites/Player/jump/001.png")
-
 
 --Draw the player
 player.draw = function()
