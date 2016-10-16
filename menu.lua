cda4ef3080bcbee5006270b650e1ef3522c0b988
Daniel Tweedy <melbrookska@user.noreply.github.com>
2016-10-16 10:51:59 -0700

    Added Menu  Logic and Credits

--- menu.lua
+++ menu.lua
@@ -1,5 +1,7 @@
 buttons = {}
-  
+X_CONSTANT = 110
+Y_CONSTANT = 30
+
 function button_spawn(x, y, text, changeState)
   table.insert(buttons, {x = x, y = y, text = text, state = changeState})
 end
@@ -7,17 +9,19 @@ end
 function button_draw()
   for i, v in ipairs(buttons) do
     love.graphics.setColor(255,255,255)
-    love.graphics.setNewFont(30)
+    love.graphics.setNewFont(Y_CONSTANT)
     love.graphics.print(v.text, v.x, v.y)
+    love.graphics.rectangle("line", v.x, v.y, X_CONSTANT, Y_CONSTANT)
   end
 end
 
 function love.mousepressed(x, y, button)
-  --print ("button: ".. button .. " x: " .. x .." y: " .. y .. " gS: " .. gameState)
+  print ("x: " .. x .." y: " .. y)
   if button == 1 and gameState == 0 then
     --print "mouse 1 pressed"
     for i, v in ipairs(buttons) do
-      if x >= v.x and x <= 300 + v.x and y >= v.y and y <= 300 + v.y then
+      if x >= v.x and x <= v.x + X_CONSTANT and y >= v.y and y <= v.y + Y_CONSTANT then
+        
         gameState = v.state
       end
     end
